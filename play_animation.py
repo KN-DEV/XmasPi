850d281e7e2232123f5430feeab599c73c65c1b9
Mateusz <zajac.mateusz@icloud.com>
2013-12-12 10:38:05 +0100

    Added downloading frames from REST app and solved bug with random diode state by repeating frame 100 times per second

--- play_animation.py
+++ play_animation.py
@@ -29,20 +29,27 @@ w.pinMode(LATCH, WRITE)
 # Amount of LEDs in XmasPi
 LED_COUNT = 40
 
+# Set fps boost rate ( real fps = fps * boost )
+BOOST_FPS = 100
+
 # Web service URL
-URL = "http://192.168.1.102/~plysiu/XmasPi-REST/index.php/animation/get.json"
+URL = "http://dev.uek.krakow.pl/~xmaspi/index.php/animation/get.json"
 
 ##################################
 
 def send_one_bit(value):
     w.digitalWrite(DATA, value)
-    
+    #t.sleep(0.02)
     w.digitalWrite(CLOCK, HIGH)
+    #t.sleep(0.02)
     w.digitalWrite(CLOCK, LOW)
+    #t.sleep(0.02)
 
 def display():
     w.digitalWrite(LATCH, HIGH)
+    #t.sleep(0.02)
     w.digitalWrite(LATCH, LOW)
+    #t.sleep(0.02)
 
 def clear(amount):
     for x in range(0, amount):
@@ -52,42 +59,54 @@ def clear(amount):
 def download_contents(url):
      return u.urlopen(url).read()
 
-def parse_json(content):
+def parse_json(contents):
     animation = json.loads(contents.decode('utf8'))
     return animation
 
-def play_animation(frames, fps=4):
-    frame_duration = 1/fps
+def play_animation(frames, fps=8):
+
+    # If error returned from REST service go with default animation
+    if  'error' in frames:
+        frames = dev.SPOLI_FRAMES
+        
+    frame_duration = 1 / fps / BOOST_FPS
     for frame in frames:
-        for state in reversed(frame):
-            send_one_bit(state)
-        display()
-        t.sleep(frame_duration)
+        for i in range(0, BOOST_FPS):
+            for state in reversed(frame):
+                send_one_bit(state)
+            display()
+            t.sleep(frame_duration)
 
-# def play_dev_animation():
-#     t.sleep(0.25)
-#     for f in dev.DEV_FRAMES:
-#         # Load registers with diod states
-#         for diod_state in reversed(f):
-#             send_one_bit( diod_state )
-#         # Discharge regsisters to light LEDs
-#         display()
-#         # Sleep to make current frame visable for a while
-#         t.sleep(0.1)
-#     #t.sleep(frame_duration * f['repetition_count'])
-#     t.sleep(0.25)
-
-# This method is lighting random amount of LEDs 
-# in random amount of time. Feel free to change the time values
 def play_christmas():
     for state in range(0, 39):
         send_one_bit(random.choice([0, 1]))
         t.sleep(random.uniform(0.1, 0.5))
         display()
+
+def play_test_moving_dot():
+    for frame in range(0, 40):
+        for dot in range(0, 40):
+            if dot == frame:
+                send_one_bit(1)
+            else:
+                send_one_bit(0)
+        display()
+        t.sleep(0.1)
     
-##################################
+####################parse_json##############
 
 while True:
-	# play_animation(dev.DEV_FRAMES)
-    # play_animation(dev.TEST_FRAMES)
-	# play_christmas()
+    #play_test_moving_dot()
+    #play_animation(dev.DEV_FRAMES)
+    #play_animation(dev.TEST_FRAMES, 4)
+    #play_animation(dev.SPOLI_FRAMES)
+    #play_animation(dev.HIGH_LOW_FRAMES)
+    #play_christmas()
+
+    if random.choice( [0, 1, 2] ):
+        play_animation( parse_json( download_contents( URL ) ) )
+    else:
+        play_animation( dev.DEV_FRAMES )
+        
+        
+    
